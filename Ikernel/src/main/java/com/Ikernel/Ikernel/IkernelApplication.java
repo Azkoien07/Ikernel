6ed273cbd356da808bd4df9e1e4066009e3e6ca8
Gabriel Cáceres <caceresgabriel305@gmail.com>
2025-03-20 10:24:35 -0500

    Creacion repositorio

---
+++ Ikernel/src/main/java/com/Ikernel/Ikernel/IkernelApplication.java
@@ -0,0 +1,13 @@
+package com.Ikernel.Ikernel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkernelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkernelApplication.class, args);
+	}
+
+}
---
+++ Ikernel/src/test/java/com/Ikernel/Ikernel/IkernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ikernel.Ikernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
